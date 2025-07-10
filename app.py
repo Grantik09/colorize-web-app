346500bcaba4099d0a1058eeb0fa17d94feae829
Grantik09 <grantik.t@gmail.com>
2025-07-10 18:23:38 +0530

    port

--- app.py
+++ app.py
@@ -68,6 +68,8 @@ def colorize():
         })
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
